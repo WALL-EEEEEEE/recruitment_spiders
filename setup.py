5b45920673cb01c792d17b08a4d8c1465ddefa02
johans <johans@debian.duanqingbb@163.com>
2018-11-05 11:20:38 +0800

    add dependency info

--- setup.py
+++ setup.py
@@ -5,6 +5,17 @@ from setuptools import setup, find_packages
 setup(
     name         = 'project',
     version      = '1.0',
-    packages     = find_packages(),
+    description  =  'Spiders of recruitment',
+    url          =  'https://github.com/duanqiaobb/recruitment_spiders',
+    author       =  'johans',
+    author_email =  '2574580419@qq.com',
+    license      =  'MIT',
+    packages     =  find_packages(),
+    install_requires = [
+        'scrapy',
+        'requests',
+        'scrapy-redis',
+        'fake_useragent'
+        ]
     entry_points = {'scrapy': ['settings = ScrapyRecruitment.settings']},
 )
